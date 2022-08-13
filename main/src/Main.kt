ae59ebf95485372fb22cbcfd0f32443a2691d180
Ahmed Al Hassan <aaa.11.26@outlook.com>
2022-08-13 17:41:56 +0300

    test commit

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -1,3 +1,30 @@
 fun main() {
-var name= "Ahmed"
+//---------variables & constants--------------------------------//
+//var name= "Ahmed"
+//    print(name)
+//
+//    name = "Ali"
+//
+//    print(name)
+//
+//    val id = 1234
+//    print(id)
+//---------naming variables & constants--------------------------//
+//    var current_speed = 20
+//    var currentSpeed = 30
+//    var _name = "Ali"
+//    var num1 = 3
+//    var n1um = 2
+//    var class_Kotlin = "Kotlin"
+//---------user inputs--------------------------------------------//
+
+//   println(" Enter your name: ")
+//    var name = readLine()
+//
+//    println("Your name is: $name")
+
+//---------data types--------------------------------------------//
+   //var num : Int = 4
+     
+
 }
